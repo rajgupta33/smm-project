b0c6b8872ec0ca8d33fc5ee7d9f5f11e7501850f
rajgupta33 <113742771+rajgupta33@users.noreply.github.com>
2025-08-17 16:14:48 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/frontend/',   // 👈 this matches your deployed path
+  base: '/',
   build: {
     outDir: 'dist',
-  },
+    emptyOutDir: true
+  }
 })
