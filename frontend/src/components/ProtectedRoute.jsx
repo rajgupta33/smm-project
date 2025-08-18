051c17719d62b3f1ba206ff5021b30649162d1a5
rajgupta33 <113742771+rajgupta33@users.noreply.github.com>
2025-08-18 13:25:10 +0530

    Update ProtectedRoute.jsx

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,9 +1,12 @@
-// ProtectedRoute.jsx
 import { Navigate, Outlet } from 'react-router-dom';
 import { useAuth } from '../context/Authcontext';
 
 const ProtectedRoute = ({ requiredRole }) => {
-  const { isAuthenticated, user } = useAuth();
+  const { isAuthenticated, user, loading } = useAuth();
+
+  if (loading) {
+    return <div>Loading...</div>; // Or a spinner
+  }
 
   if (!isAuthenticated) return <Navigate to="/login" />;
   if (requiredRole && user.role !== requiredRole) return <Navigate to="/unauthorized" />;
